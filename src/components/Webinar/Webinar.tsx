e3b769921575bf5f8c1ab6ec6ffd528684f13eb0
Ari Rahim <aritomsk@yahoo.com>
2022-05-04 18:10:20 +0300

    test safari

--- src/components/Webinar/Webinar.tsx
+++ src/components/Webinar/Webinar.tsx
@@ -1,3 +1,4 @@
+
 import React, { FC } from "react"
 import "./Webinar.scss"
 // import WebinarImg from "../../images/Webinar.png"
@@ -24,14 +25,13 @@ const Webinar: FC<ComponentProps> = ({ handleWebinarClose, style }) => {
         <div style={{height: '50px', padding: "50px"}}>
             {/* {window.screen.width > 1000 ? ( */}
                 <div className="whatIsWalletBtn">
-                <button
+                <p
                     className="whatIsWalletBtn link-btn"
-                    type="button"
                     onClick={() => {
                     }}
                 >
                     What is a wallet?
-                </button>
+                </p>
             </div>
             {/* ) : null} */}
         </div>
@@ -39,44 +39,3 @@ const Webinar: FC<ComponentProps> = ({ handleWebinarClose, style }) => {
 }
 
 export default Webinar
-
-// import React, { FC } from "react"
-// import "./Webinar.scss"
-// // import WebinarImg from "../../images/Webinar.png"
-
-// const oneDayInSeconds = 86400;
-// const startingDateTimeStamp = 1650608400; // 2022-04-22 00:00:00 GMT
-
-// interface ComponentProps {
-//     handleWebinarClose: any
-//     style: any
-// }
-
-// const Webinar: FC<ComponentProps> = ({ handleWebinarClose, style }) => {
-
-//     const bookMySeat = () => {
-//         let currentTime = Date.now()/1000;
-//         let timeDiff = currentTime - startingDateTimeStamp;
-//         let diffInDays = timeDiff / oneDayInSeconds;
-//         let refId = Math.floor(diffInDays + 4) ;
-//         window.open(`https://superworld.eventcalendarapp.com/u/33679/162282?repeatId=${refId}`, "_blank");
-//     }
-
-//     return (
-//         <div style={{height: '50px', padding: "50px"}}>
-//             {/* {window.screen.width > 1000 ? ( */}
-//                 <div className="whatIsWalletBtn">
-//                 <p
-//                     className="whatIsWalletBtn link-btn"
-//                     onClick={() => {
-//                     }}
-//                 >
-//                     What is a wallet?
-//                 </p>
-//             </div>
-//             {/* ) : null} */}
-//         </div>
-//     )
-// }
-
-// export default Webinar
